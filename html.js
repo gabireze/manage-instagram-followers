9f296a87ded3441e660d71aa17234e45a230f5dd
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2024-07-29 16:27:38 -0300

    ensure splash-screen is the first div inside body and insert component HTML correctly

--- html.js
+++ html.js
@@ -1,34 +1,37 @@
 const init = () => {
-  var targetElement = document.getElementById("splash-screen");
-  if (targetElement) {
-    var componentHtml = `
-    <div class="overlay" id="overlay">
-    <div class="my-component centered-column">
-      <h2>Manage Instagram Followers</h2>
-      <div id="info-text">
-        Manage your Instagram followers, including features to follow back,
-        unfollow non-followers, and discover mutual followers.
-      </div>
-      <input type="text" placeholder="Search..." id="searchInput" />
-      <div id="titleAndFilter">
-        <h3 id="title"></h3>
-        <button id="filterNotFollowingBackButton" class="filter">
-          Filter Non-Followers
-        </button>
-        <button id="filterNotFollowedBackButton" class="filter">
-          Filter Not Followed Back
-        </button>
-      </div>
-      <div class="user-list" id="userList"></div>
-      <button id="loadFollowingButton">Following</button>
-      <button id="loadFollowersButton">Followers</button>
-      <div class="loader" id="loader"></div>
-    </div>
-  </div>  
-  `;
-    targetElement.insertAdjacentHTML("beforebegin", componentHtml);
-  } else {
-    console.error("Target element not found");
+  let targetElement = document.getElementById("splash-screen");
+  if (!targetElement) {
+    targetElement = document.createElement("div");
+    targetElement.id = "splash-screen";
+    document.body.insertBefore(targetElement, document.body.firstChild);
   }
+
+  var componentHtml = `
+    <div class="overlay" id="overlay">
+      <div class="my-component centered-column">
+        <h2>Manage Instagram Followers</h2>
+        <div id="info-text">
+          Manage your Instagram followers, including features to follow back,
+          unfollow non-followers, and discover mutual followers.
+        </div>
+        <input type="text" placeholder="Search..." id="searchInput" />
+        <div id="titleAndFilter">
+          <h3 id="title"></h3>
+          <button id="filterNotFollowingBackButton" class="filter">
+            Filter Non-Followers
+          </button>
+          <button id="filterNotFollowedBackButton" class="filter">
+            Filter Not Followed Back
+          </button>
+        </div>
+        <div class="user-list" id="userList"></div>
+        <button id="loadFollowingButton">Following</button>
+        <button id="loadFollowersButton">Followers</button>
+        <div class="loader" id="loader"></div>
+      </div>
+    </div>
+  `;
+  targetElement.insertAdjacentHTML("beforebegin", componentHtml);
 };
+
 init();
